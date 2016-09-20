ce093e74a1563d0ea260fb86f66ffbed2a801420
Thomas <jtruant@gmx.com>
2016-09-20 00:26:48 -0600

    color resume_url link

--- list.php
+++ list.php
@@ -9,10 +9,20 @@ $entries = $applicants['entries'] or die('Bork');
 </head>
 <body>
     <b>Applicants: </b>
+    <div class="applicants">
     <?php foreach($entries as $entry): ?>
-        <pre class="entry">
-<?= json_encode($entry, JSON_PRETTY_PRINT); ?>
-        </pre>
+        <div class="entry">
+        {
+            <?php foreach($entry as $key=>$value): ?>
+            <div class="<?php echo $key; ?>">
+                &nbsp;&nbsp;&nbsp;&nbsp;"<?php echo $key; ?>": 
+                "<span class="value"><?php echo $value; ?></span>"
+            </div>
+            <?php endforeach; ?>
+        }
+        </div>
     <?php endforeach; ?>
+    </div>
+    <script src="static/app.js"></script>
 </body>
 </html>
