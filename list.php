692d3fec02c55476000eeccd55226f2d5c603897
Thomas <jtruant@gmx.com>
2016-09-19 22:56:23 -0600

    print applicants

--- list.php
+++ list.php
@@ -1,3 +1,18 @@
 <?php
-
+require_once('json.php');
+$applicants = read_json('applicants.json', true);
+$entries = $applicants['entries'] or die('Bork');
 ?>
+<html>
+<head>
+    <title>Applicants</title>
+</head>
+<body>
+    <b>Applicants: </b>
+    <?php foreach($entries as $entry): ?>
+        <pre class="entry">
+<?= json_encode($entry, JSON_PRETTY_PRINT); ?>
+        </pre>
+    <?php endforeach; ?>
+</body>
+</html>
